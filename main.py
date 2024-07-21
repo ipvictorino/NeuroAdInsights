4a70ba2aa0bb9271e95043d2645c00b9502c1657
Inês Victorino <invfi1997@gmail.com>
2024-07-21 21:13:51 +0100

    First version FastAPI - Formatted

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from orchestrator import LangChainWorkflow
 from fastapi import FastAPI, HTTPException, UploadFile, File, Depends
 from pydantic import BaseModel
 from pathlib import Path
@@ -8,17 +9,19 @@ from typing import Optional
 
 logging.basicConfig(level=logging.INFO)
 
-from orchestrator import LangChainWorkflow
 
 app = FastAPI()
+
 
 class ImageNames(BaseModel):
     image_name: Optional[str] = None
     heatmap_name: Optional[str] = None
 
+
 async def resize_image(self, image_data):
     logging.error("Image size is too large. Must be less than 20 MB in size.")
     return image_data  # TODO: Implement resizing logic
+
 
 async def load_image(self, images_dir, image_path):
     image_file_path = images_dir / image_path
@@ -27,14 +30,17 @@ async def load_image(self, images_dir, image_path):
         if len(image_data) > 20 * 1024 * 1024:
             image_data = self.resize_image(image_data)
         return base64.b64encode(image_data).decode('utf-8')
-    
+
+
 async def read_image_file(file: UploadFile):
     logging.info(f"Reading image file: {file.filename}...")
     image_data = await file.read()
     if len(image_data) > 20 * 1024 * 1024:
-        logging.error("Image size is too large. Must be less than 20 MB in size.")
+        logging.error(
+            "Image size is too large. Must be less than 20 MB in size.")
         # TODO: Implement resizing logic if needed
     return base64.b64encode(image_data).decode('utf-8')
+
 
 @app.post("/process")
 async def process_image(
@@ -45,12 +51,11 @@ async def process_image(
 ):
 
     images_dir = Path(__file__).parent / "data" / "images"
-    
-    
+
     if image_name and heatmap_name:
         logging.info(f"Loading image and heatmap from file names")
         try:
-            image_base64_str = await load_image( images_dir, image_name)
+            image_base64_str = await load_image(images_dir, image_name)
             heatmap_base64_str = await load_image(images_dir, heatmap_name)
             logging.info(f"Image and heatmap files read successfully")
 
@@ -59,21 +64,27 @@ async def process_image(
     elif image_file and heatmap_file:
         logging.info(f"Loading image and heatmap from file uploads")
         try:
-            logging.info(f"Received image_file: {image_file.filename}, size: {image_file.size}")
-            logging.info(f"Received heatmap_file: {heatmap_file.filename}, size: {heatmap_file.size}")
+            logging.info(
+                f"Received image_file: {image_file.filename}, size: {image_file.size}")
+            logging.info(
+                f"Received heatmap_file: {heatmap_file.filename}, size: {heatmap_file.size}")
             image_base64_str = await read_image_file(image_file)
             heatmap_base64_str = await read_image_file(heatmap_file)
             logging.info(f"Image and heatmap files read successfully")
         except Exception as e:
-            raise HTTPException(status_code=400, detail="Error processing files")
+            raise HTTPException(
+                status_code=400, detail="Error processing files")
     else:
         logging.error("Image and heatmap files are required")
         # logs input args
-        logging.info(f"{image_name}, {heatmap_name}, {image_file}, {heatmap_file}")
-        raise HTTPException(status_code=400, detail="Image and heatmap files are required")
-    
+        logging.info(
+            f"{image_name}, {heatmap_name}, {image_file}, {heatmap_file}")
+        raise HTTPException(
+            status_code=400, detail="Image and heatmap files are required")
+
     if not image_base64_str or not heatmap_file:
-        raise HTTPException(status_code=400, detail="Image and heatmap files are required")
+        raise HTTPException(
+            status_code=400, detail="Image and heatmap files are required")
     # Initialize the LangChainWorkflow
     workflow = LangChainWorkflow(
         openai_api_key=os.getenv("OPENAI_API_KEY"),
@@ -82,7 +93,7 @@ async def process_image(
     # return ["answer1", "answer"]
     response_a, response_b, response_c = workflow.run(
         image_base64_str, heatmap_base64_str)
-    
+
     logging.info(f"Processed images.")
 
     return {

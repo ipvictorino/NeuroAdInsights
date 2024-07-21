2f6f2916b0ced9cc4da936feb68dff99f32da06a
Inês Victorino <invfi1997@gmail.com>
2024-07-21 21:14:32 +0100

    Updates streamlit to integrate with local server

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,11 +1,54 @@
 import streamlit as st
-# from langchain.workflow import run_workflow
+import requests
+from streamlit_extras.stylable_container import stylable_container
 
 st.title("🦜🔗 - Neuromarketing Insights")
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png"])
-uploaded_heatmap = st.file_uploader("Upload corresponding attention heatmap", type=["jpg", "png"])
+uploaded_heatmap = st.file_uploader(
+    "Upload corresponding attention heatmap", type=["jpg", "png"])
 
-# if uploaded_image and uploaded_heatmap:
-#     # Process the inputs and run the workflow
-#     result = run_workflow(uploaded_image, uploaded_heatmap)
-#     st.json(result)
+if st.button("Analyze"):
+    if uploaded_image is not None and uploaded_heatmap is not None:
+        with st.spinner("Your Ad Insights are being generated. Please hold tight...", ):
+            files = {
+                "image_file": (uploaded_image.name, uploaded_image, uploaded_image.type),
+                "heatmap_file": (uploaded_heatmap.name, uploaded_heatmap, uploaded_heatmap.type)
+            }
+
+            response = requests.post(
+                "http://localhost:8000/process", files=files)
+
+            if response.status_code == 200:
+                result = response.json()
+
+                st.success(body="Success! ", icon="🎉")
+
+                with st.expander("See Results"):
+
+                    col1, col2 = st.columns(2)
+                    with col1:
+                        st.image(uploaded_image, caption="Uploaded Image",
+                                 use_column_width=True)
+                    with col2:
+                        st.image(
+                            uploaded_heatmap, caption="Uploaded Heatmap", use_column_width=True)
+
+                    with stylable_container(
+                        "codeblock",
+                        """
+                        code {
+                            white-space: pre-wrap !important;
+                        }
+                        """,
+                    ):
+                        st.markdown("### Main Insights")
+                        st.markdown(f"**Insight A - Saliency Description:**")
+                        st.markdown(result['response_a'])
+                        st.markdown(f"**Insight B - Cognitive Description:**")
+                        st.markdown(result['response_b'])
+                        st.markdown(f"**Detailed Summary:**")
+                        st.markdown(result['response_c'])
+            else:
+                st.error("Error processing the files")
+    else:
+        st.error("Please upload an image and a heatmap file")
